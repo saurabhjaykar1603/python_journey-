9099b15ff0a435b9a1e0348374e20255bee57348
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-08 22:23:09 +0530

    learn- Rules to declare a variable in python

---
+++ Day 3 Rules to declare a variable in python/main.py
@@ -0,0 +1,8 @@
+# Rules to declare a variable in python
+
+
+# A variable name must start with a letter a-z or A-Z or the underscore _ character.
+# A variable name cannot start with a number.
+# Variable names are case-sensitive, which means name and Name are considered different variables.
+# A variable name can not used reserved keyword
+# A variable name can include a-z,A-z, 0-9, and _ Here are some examples of valid and invalid variable names:
