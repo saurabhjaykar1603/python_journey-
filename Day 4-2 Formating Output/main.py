c00083771c5c259e9a1c730716753937293a1447
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-09 23:09:27 +0530

    Learn - formate fun

---
+++ Day 4-2 Formating Output/main.py
@@ -0,0 +1,10 @@
+# formating output it means our personalized output
+
+studentName = "Saurabh"
+age= 21
+dob = "16-03-2002"
+print('Hello',studentName," Your Age is",age, " Your date of Birth is",dob)
+
+# there are best way is formate function
+# with the Help of  -----  {}
+print("Hello {place1} Your Age is {place2} Your date of Birth is {place3}".format(place1=studentName,place2=age,place3=dob))
