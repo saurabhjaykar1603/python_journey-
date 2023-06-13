b2c3f2c625f5ce6fd608245784dc4a9ec636fa8a
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-13 08:39:27 +0530

    learned - input func with google collabe

--- Day 5 Taking input from user/main.py
+++ Day 5 Taking input from user/main.py
@@ -1,4 +1,16 @@
 name = input("Enter Your name")
 print(name)
 
-# You Should Use google collage web based editore for output 
+# You Should Use google collage web based editore for output
+
+val1 =int(  input ("enter value 1"))
+val2 =int(  input ("enter value 2"))
+
+sum= val1+val2
+
+print('sum of {} and {} is {}'.format(val1,val2,sum))
+
+# copy Following link to use input function with the help of google collabe
+
+# https://colab.research.google.com/drive/1JnS7jh8Tbtyv0gCUANNsHC6CqGdXoy2O?usp=sharing
+
