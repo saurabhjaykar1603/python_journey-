2914b27e9099d19f8cf5c4a3014644a21fadcb6b
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-13 08:51:09 +0530

    learned - Swapping value to eachother

---
+++ Day 5 Taking input from user/swaping.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+print("Before Swapping value of a", a)
+print("Before Swapping value of a", b)
+
+
+temp = b
+
+b =  a
+
+a = temp
+
+print("After Swapping value of a", a)
+print("After Swapping value of a", b)
+
