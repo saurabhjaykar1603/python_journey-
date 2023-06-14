3090c80426b7c0ec2c66f8f469d3750293ec9f5a
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-14 06:30:53 +0530

    lerned - multification func wit ex

---
+++ Day 5 Taking input from user/HW.py
@@ -0,0 +1,8 @@
+rate = input()
+quantity = input()
+bill =int(rate) * int(quantity)
+print("rate is {}, quantity is {} total bill is {}".format(rate,quantity,bill))
+
+# copy Following link to use input function with the help of google collabe
+
+# https://colab.research.google.com/drive/1fAz414UsFuHtHNNpiNk6F2AvkweV1-Ho?usp=sharing
