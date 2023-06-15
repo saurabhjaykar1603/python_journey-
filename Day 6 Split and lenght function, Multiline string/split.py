a0620d9b50daa29ade0fdac7669018e0e6765c44
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-15 14:15:10 +0530

    lerned - split func with multiple input

--- Day 6 Split and lenght function, Multiline string/split.py
+++ Day 6 Split and lenght function, Multiline string/split.py
@@ -1,2 +1,12 @@
 sentence = "My Name is Pinky".split()
 print(sentence)
+
+# split() fun ussase in multiple input 
+
+a, b = input ("Enter 2 Numbers :").split() 
+print("a =",a)
+print("b =",b)
+
+# use this google collabe link for output
+
+# https://colab.research.google.com/drive/13a-aSTSVUN2yLGpdSsH0kM17r7Whn1s1?usp=sharing
