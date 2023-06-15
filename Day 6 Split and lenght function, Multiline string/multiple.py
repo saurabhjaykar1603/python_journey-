285741a133037467ba7fd10505ee46938e8a2d1d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-15 16:00:31 +0530

    lerned -how to  multiple line print

---
+++ Day 6 Split and lenght function, Multiline string/multiple.py
@@ -0,0 +1,16 @@
+print("""SONG LYRICS
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.
+
+Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.""")
+
+# whenever we want to print multipleline code we should use  """.........."""
