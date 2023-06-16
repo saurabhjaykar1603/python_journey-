1cf225b7c835e895ed5f05441deaa8e0fa2b38e3
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-16 08:00:14 +0530

    lerned- Upper case

--- Day 7 Comments and String Operations/operation.py
+++ Day 7 Comments and String Operations/operation.py
@@ -6,3 +6,10 @@ myString = 'road to code 4u'
 newString = myString.upper()
 print("orginal string",myString)
 print(newString)
+
+
+# 2.lower ()
+print(newString)
+String = newString.lower()
+print(String)
+
