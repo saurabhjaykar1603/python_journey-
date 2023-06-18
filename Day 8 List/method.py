b570b82498694e064b7a6de13a0a51d46d064c5d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-18 08:20:53 +0530

    learned - LIst Append Method

---
+++ Day 8 List/method.py
@@ -0,0 +1,6 @@
+# Append
+# The append() method adds an element to the end of the list.courses = ["c","c++","python","java","icp"]
+courses = ["c","c++","python","java","icp"]
+print("Before append :",courses)
+courses.append("Android Dev")
+print("After append :",courses)
