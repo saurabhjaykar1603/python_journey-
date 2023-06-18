fee517ff73710457056bee7f702f5462fa94c0e1
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-18 08:24:12 +0530

    learned - POP Method

--- Day 8 List/method.py
+++ Day 8 List/method.py
@@ -11,3 +11,12 @@ fruits = ['appple','banana','orange','cherry']
 print("Before remove",fruits)
 fruits.remove('banana')
 print("After remove",fruits)
+
+
+# Pop
+# The pop() method remove the element. It takes a single argument (index).
+
+fruit = ['appple','banana','orange','cherry']
+print("Before pop",fruit)
+fruit.pop(2)
+print("After pop",fruit)
