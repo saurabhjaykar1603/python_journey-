11849d236cfacf707c453c049ceaf016c4122a46
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-18 08:18:35 +0530

    learned - LIst is Updatable

--- Day 8 List/main.py
+++ Day 8 List/main.py
@@ -2,3 +2,9 @@
 
 students = ['Aachal','Aarav','Avinash','Dolly','Deva']
 print(students)
+
+# The list is changeable and we can change add and remove items in a list after it has been created
+courses = ["c","c++","python","javascript","icp"]
+print("Befor update: ",courses)
+courses[1] = "c++ programming"
+print("After update :",courses)
