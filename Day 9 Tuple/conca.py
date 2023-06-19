f1d69cb485f87a1a288c711573d5b22f22ff04b0
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-19 22:46:00 +0530

    learned - to concatenate, or combine, two strings you can use the + operator.

---
+++ Day 9 Tuple/conca.py
@@ -0,0 +1,7 @@
+# Concatenation of String
+# To concatenate, or combine, two strings you can use the + operator.
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
--- Day 9 Tuple/main.py
+++ Day 9 Tuple/main.py
@@ -14,3 +14,12 @@ listfastfood = list(fastfood)
 listfastfood[1] = "Edli"
 fastfood = tuple(listfastfood)
 print(fastfood)
+
+# Index Position
+fastfood = ("Vadapav","Pizza","Burger","Dosa","PaniPuri")
+print(fastfood.index("Pizza"))
+
+# Count()
+# The count() method returns the number of times the specified element appears in the list.
+fastfood = ("Vadapav","Pizza","Burger","Pizza","PaniPuri")
+print(fastfood.count("Pizza"))
