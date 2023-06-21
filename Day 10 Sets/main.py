f3bc679b9c60bdbbc741730b3b89bbda4df28b03
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-21 09:26:12 +0530

    learned - set and unorder property

---
+++ Day 10 Sets/main.py
@@ -0,0 +1,10 @@
+# Sets :
+# A set in Python is an unordered collection of unique items. It is a data type that acts like a mathematical set. It is defined by enclosing the items within curly braces {} and each item seperated by (,).
+
+# Properties of Sets
+
+# Unorderd :
+# Unordered means that the items in a set are not stored in any particular order.
+
+mycolors = {'sky','pink','yellow','green'}
+print(mycolors)
