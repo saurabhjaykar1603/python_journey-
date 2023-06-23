f51b9a615a9cf3435e98e23ddea8a8f96e76b0f4
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-23 16:09:54 +0530

    learned -  data values in key:value pairs

---
+++ Day 11 Dictionary/main.py
@@ -0,0 +1,9 @@
+# Dictionary in Python
+# Dictionaries are used to store data values in key:value pairs.
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+print(phoneBook)
