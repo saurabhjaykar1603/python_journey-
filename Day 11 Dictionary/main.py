998fd569a1ef21a00f78a0249c305dd89b1bb8d9
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-23 16:13:40 +0530

    learned -  get method

--- Day 11 Dictionary/main.py
+++ Day 11 Dictionary/main.py
@@ -7,3 +7,15 @@ phoneBook = {
     "Pooja" : "878975457"
 }
 print(phoneBook)
+
+# Example by using get() method :
+
+# The code defines a dictionary named phoneBook which contains key-value pairs where each key represents a name and each value represents a phone number. The code then prints in a formatting way the value associated with the key "Anu" using a dictionary.
+
+phoneBook = {
+    "Dolly" : "1234567890",
+    "Anu" : "9876543210",
+    "Avinash" : "21323422222",
+    "Pooja" : "878975457"
+}
+print(phoneBook.get("Avinash"))
