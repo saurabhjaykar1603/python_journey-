55034e06bfb3ba38072b09b29b9e60ff5cf3e416
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-24 10:15:28 +0530

    learned - equal to operator

---
+++ Day 12.1 Comparision Op/main.py
@@ -0,0 +1,12 @@
+# Comparison Operators / Relational Operators 
+# 1. equals to (==) - this operator return true both oprands are euqal
+a = 1
+b =1
+result = a==b
+print(result)
+
+# false Statement
+a = 1
+b =2
+result = a==b
+print(result)
