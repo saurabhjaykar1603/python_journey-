0bc04bdd00db11845dfdc5eaddf3473e6a93607e
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-25 09:20:53 +0530

    learned - if statement

---
+++ Day 12.2 if-else/main.py
@@ -0,0 +1,12 @@
+
+# Indentation
+
+# Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important. Python uses indentation to indicate a block of code.
+
+# conditional Statement 
+# 1 if statement 
+
+a = 20
+b = 20
+if a == b:
+    print("a is equal to b")
