19aae910134e2bf0fd0653d7793ebde0c08a0aaa
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-26 10:14:37 +0530

    learned - if else statement with example even and odd number

--- Day 12.2 if-else/main.py
+++ Day 12.2 if-else/main.py
@@ -34,3 +34,15 @@ if YourAge>=RequireAge:
   print("Your eligible for voting")
 else:
   print("Your not eligible for voting")
+
+
+#  Write a program to check whether entered number is even ir odd by using if-else statements.
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
+
+    # https://colab.research.google.com/drive/1xltjEzbwh9_C8A65LI_s3cWvt2eY4icD#scrollTo=dbgjLGfhySqU&line=2&uniqifier=1
