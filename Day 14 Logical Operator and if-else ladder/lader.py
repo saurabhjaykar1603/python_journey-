467bb6ab0fdb119798592317635874be6c8a825d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-28 23:19:05 +0530

    initial changes

--- Day 14 Logical Operator and if-else ladder/lader.py
+++ Day 14 Logical Operator and if-else ladder/lader.py
@@ -16,5 +16,7 @@ elif num==5:
 else :
   print("You selected wrong input")
 
+#   In the above example we create one num variable and in that variable we give input number from the user and checks whether the number is between 1 to 5. If the input is within the range, it displays the corresponding message, and if the input is out of range, it displays You selected wrong input.
+
 # use following google collab link to implement code 
 #   https://colab.research.google.com/drive/1JpKjnZsRuWiJ1eFwDFAQA3_Wfb6Fjw7z?usp=sharing
