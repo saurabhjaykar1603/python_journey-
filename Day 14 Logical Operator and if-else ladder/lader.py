f613e5ddc20982222f18191eedcd4ee0a69d39be
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-28 23:14:36 +0530

    learned - if-else-lader statement  using input  number 1 to 5

---
+++ Day 14 Logical Operator and if-else ladder/lader.py
@@ -0,0 +1,20 @@
+# If else Ladder
+# Python elif is used to execute a continuous chain of conditional logic ladder. In elif,
+# there are multiple conditions and the corresponding statements as a ladder.
+
+num = int(input("Enter number from 1 to 5 :"))
+if num==1:
+  print("You selected option 1")
+elif num==2:
+  print("You selected option 2")
+elif num==3:
+  print("You selected option 3")
+elif num==4:
+  print("You selected option 4")
+elif num==5:
+  print("You selected option 5")
+else :
+  print("You selected wrong input")
+
+# use following google collab link to implement code 
+#   https://colab.research.google.com/drive/1JpKjnZsRuWiJ1eFwDFAQA3_Wfb6Fjw7z?usp=sharing
