83ec54f07553d0f6a1ce8d8a9ebee634e6f52fc7
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-28 09:57:12 +0530

    learned - logical 'and operator and

---
+++ Day 14 Logical Operator and if-else ladder/logical.py
@@ -0,0 +1,22 @@
+# Logocal Operators to use  Combine the Condtional statement
+
+#AND operator
+# result true : if both operation condtions are true otherwise False
+# example
+
+# HSC>60 and JEE>120 
+HSC = 50
+JEE = 140
+result =HSC>60 and JEE>120 
+print(result)
+
+# output = False
+
+HSC = 80
+JEE = 140
+result =HSC>60 and JEE>120 
+print(result)
+
+# output =True
+
+
