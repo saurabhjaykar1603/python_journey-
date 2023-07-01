2829f4ad5097bd7411d99879a5f683f4bd856892
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-01 17:35:41 +0530

    learned - for loop defination

---
+++ Day 16 For Loop/main.py
@@ -0,0 +1,3 @@
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+
+# This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
