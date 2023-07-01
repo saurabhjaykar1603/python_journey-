991f9887710a793c1e44767e3e5845e78cf1ae74
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-01 17:46:34 +0530

    learned - for loop syntx and example

--- Day 16 For Loop/main.py
+++ Day 16 For Loop/main.py
@@ -1,3 +1,10 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
 # This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+
+# # range(start, stop, step) syntx
+# for loop ar used to when you have a block of code which you want 
+# to repeat a fixed number of time
+
+for i in range(10):
+    print("hello worls")
