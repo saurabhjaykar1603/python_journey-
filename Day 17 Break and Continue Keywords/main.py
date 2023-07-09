f3bbed51f18746542681ab68bc222bef74e45a71
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-09 23:53:20 +0530

    learned - else keyword in python

--- Day 17 Break and Continue Keywords/main.py
+++ Day 17 Break and Continue Keywords/main.py
@@ -13,3 +13,14 @@ for val in range(10):
  if(val)==2:
   continue
  print(val)
+
+
+
+# else keyword in python
+# example
+for i in range(10):
+  print(i)
+else :
+  print("Loop Executed successfully")
+  
+
