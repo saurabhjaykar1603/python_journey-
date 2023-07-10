5590c3d567caf65c3d7f6f39ec3ec7d000ef35b9
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-10 22:48:10 +0530

    learned -program to count the number of digit present in a number

--- Day 18 while loop/main.py
+++ Day 18 while loop/main.py
@@ -10,3 +10,17 @@ i = 0
 while i<5:
     print(i)
     i = i+1
+
+# write a program to count the number of digit present in a number
+num = int(input("Enter Any Number : "))
+count = 0
+while num>0:
+    num = num //10
+    count = count +1
+print("Total Digit :", count)
+
+# fellow use google colab link 
+# https://colab.research.google.com/drive/1skxDSTMO_UIenfMJMJpXTvd7A5WBXTOo?usp=sharing
+
+
+
