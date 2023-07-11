567d41266a0f857dc33d595fbef4e3f870f25d73
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-11 08:50:44 +0530

    learned - function is a block of code which only run when its calls

---
+++ Day 19 Function/main.py
@@ -0,0 +1,12 @@
+# function in python 
+# function is a block of code which only run when its calls
+
+# Example : 
+
+def greeting():
+    print("Good Morning Everyone")
+    print("Good Evening Everyone")
+    print("Good night Everyone")
+greeting()
+greeting()
+greeting()
