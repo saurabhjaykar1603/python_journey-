651816bc630d7584b6fce91c72feba4e2f01d43f
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-15 23:56:17 +0530

    learned - datetime modules

--- Day 22 Math Modules/main.py
+++ Day 22 Math Modules/main.py
@@ -15,11 +15,17 @@ print(newNumber1)
 
 # math.sqrt() method 
 # to find squareroot 
-import math 
-num = int(input("Enter number...."))
-ans = math.sqrt(num)
-print(ans)
+# import math 
+# num = int(input("Enter number...."))
+# ans = math.sqrt(num)
+# print(ans)
 # https://colab.research.google.com/drive/1ZZvq5sWcw7xTePtTi0tp7UriPxBXm0jJ?usp=sharing
+
+# date and time module 
+import datetime
+currentDate = datetime.datetime.now()
+print(currentDate)
+
 
 
 
