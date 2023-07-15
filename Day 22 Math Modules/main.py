c5dce7cbe2bb7a9125f5a6c45f22d2bcfd0bcb9d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-07-15 23:38:32 +0530

    learned - math floor modules

--- Day 22 Math Modules/main.py
+++ Day 22 Math Modules/main.py
@@ -1,7 +1,14 @@
 # math modules 
 # math.ceil
 import math 
-num = 12.3
-num = 120.3
+
+num = 120.8
 newNumber = math.ceil(num)
 print(newNumber)
+
+# math.floor
+# nearest downword num 
+import math 
+num1 = 120.8
+newNumber1 = math.floor(num1)
+print(newNumber1)
